2cb85649f608a13b4794e1e6ed56afdc2e877795
anjalishiriyammanavar <ap4832690@gmail.com>
2025-02-20 12:37:00 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Button from "./components/Button";
+import ProductCard from "./components/ProductCard";
+import Counter from "./components/Counter";
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import House from './pages/house';
+import Pro from './pages/Pro';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/hello" element={<House/>}/>
+            <Route path="/hi" element={<Pro/>} />
+            
+        </Routes>
+        </BrowserRouter> 
+    );
 }
 
-export default App
+
+//function App() {
+  //const products= [
+    //{
+     // name: "Laptop",
+     // price: 50000,
+    //},
+   // {
+      //name: "Mobile",
+     // price: 70000,
+   // },
+ // ];
+
+  //return (
+    //<>
+     // <h1>Anjali</h1>
+     // <Button/>
+     // <Button/>
+     // {products.map((products) => (
+       // <ProductCard name={products.name} price={products.price}
+       // />
+    //  ))}
+     // {/* <ProductCard name="Laptop" price={50000}/>
+      // <ProductCard name="Mobile" price={70000}/> */}
+      // <Counter />
+      // </>
+ // );
+//}
+
+export default App;
+       
---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect,useState } from "react";
+
+const Counter = () => {
+    const[count,setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count +1);  
+};
+
+const decrement = () => {
+    setCount(count - 1);
+};
+
+useEffect(()=>{
+    console.log("Inside effect");
+},[count]);
+
+return(
+    <div>
+        <h1>{count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+    </div>
+);
+};
+
+export default Counter;
